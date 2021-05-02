2fd6298f92f1f72a3857371b014e728fcac25397
Rasika Maduranga <rasikat77@gmail.com>
2021-05-02 14:39:13 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+const IndexPage = () => (
+  <div className="text-3xl text-red-700 bg-yellow-100 p-40">
+    <div className="w-40 animate-spin ">
+      Tailwind
+    </div>
+    <div className="w-40 animate-spin text-green-700">
+      NextJs
+    </div>
+  </div>
+)
+
+export default IndexPage
